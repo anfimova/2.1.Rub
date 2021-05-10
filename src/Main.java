764d0864542241c8c1b9deaa8d73bf3ebd157164
a.anfimova <a.anfimova@cardsmobile.ru>
2021-05-10 21:59:03 +0300

    Условие insert > 1000 вынесено выше в расчёт бонуса

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        float beginBalance = 100;
-        float insert = 1100;
-        int bonus = (int) (insert / 100);
-        float balanceWithBonus = (insert >= 1000) ? (beginBalance + insert + bonus) : (beginBalance + insert);
+        int beginBalance = 100;
+        int insert = 1100;
+        int bonus = insert > 1000 ? insert / 100 : 0;
+        int balanceWithBonus = beginBalance + insert + bonus;
+
         System.out.println("Начальный баланс: " + beginBalance);
         System.out.println("Внесено: " + insert);
         System.out.println("Бонусов: " + bonus);
